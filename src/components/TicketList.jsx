08aa9d3051a1388c5826f8c3bbe6fa7cb47f6496
Rob McKenzie <mckenro@gmail.com>
2017-09-25 08:38:43 -0700

    add initial files

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(props){
+  return(
+    <Ticket
+      location="3A"
+      names="Team Rob"
+      issue="Firebase won't save record..."/>
+  );
+}
+
+export default TicketList;

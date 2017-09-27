fabaf6159bdda7d3cb64584dd7423d5a27c8ffec
Rob McKenzie <mckenro@gmail.com>
2017-09-27 09:15:19 -0700

    add webpack-server and configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,13 @@ import Header from "./Header";
 import TicketList from "./TicketList";
 
 function App(props){
+  var myStyledComponentsStyles = {
+    backgroundColor: "#ecf0f1",
+    fontFamily: "sans-serif",
+    paddingTop: "50px"
+  }
   return (
-    <div>
+    <div style={myStyledComponentsStyles}>
       <Header/>
       <TicketList/>
     </div>
